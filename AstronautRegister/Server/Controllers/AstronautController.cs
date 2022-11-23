657057114f7e98932748b2d0f69e92c741813d43
a_michalek <a_michalek@utb.cz>
2022-11-23 20:21:33 +0100

    First commit

---
+++ AstronautRegister/Server/Controllers/AstronautController.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AstronautRegister.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AstronautController : ControllerBase
+{
+    
+}
