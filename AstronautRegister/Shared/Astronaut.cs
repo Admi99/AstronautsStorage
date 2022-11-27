400ba19458e766fb3a9a3a9c3a3bcef5787dcbef
a_michalek <a_michalek@utb.cz>
2022-11-27 10:48:36 +0100

    Add index page & azure settings

---
+++ AstronautRegister/Shared/Astronaut.cs
@@ -0,0 +1,12 @@
+﻿namespace AstronautRegister.Shared;
+
+public record Astronaut
+{
+    public string Name { get; set; } = string.Empty;
+    
+    public string Surname { get; set; } = string.Empty;
+
+    public DateTimeOffset DateOfBirth { get; set; }
+
+    public string Superpower { get; set; } = string.Empty;
+}
