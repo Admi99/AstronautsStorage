8b3bb7d086adce0f6d8bded33244041967bdc364
a_michalek <a_michalek@utb.cz>
2022-11-30 13:46:43 +0100

    Add modal and refactor stuff

--- AstronautRegister/Shared/Astronaut.cs
+++ AstronautRegister/Shared/Astronaut.cs
@@ -1,14 +1,23 @@
-﻿namespace AstronautRegister.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AstronautRegister.Shared;
 
 public record Astronaut
 {
     public int Id { get; init; }
     
+    [Required]
+    [StringLength(49, ErrorMessage = "First name is too long")]
     public string Name { get; set; } = string.Empty;
     
+    [Required]
+    [StringLength(49, ErrorMessage = "Last name is too long")]
     public string Surname { get; set; } = string.Empty;
 
+    [Required]
     public DateTimeOffset DateOfBirth { get; set; }
 
+    [Required]
+    [StringLength(49, ErrorMessage = "Superpower is too long")]
     public string Superpower { get; set; } = string.Empty;
 }
